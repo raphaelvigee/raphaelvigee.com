f4579fc2e6bf08ebaa3ec2c3da09b9303fb3dcbb
Raphael Vigee <contact@raphaelvigee.com>
2018-11-24 23:36:18 -0800

    Add CircleCI

---
+++ .circleci/deploy.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+git config --global user.email $GH_EMAIL
+git config --global user.name $GH_NAME
+
+git clone $CIRCLE_REPOSITORY_URL out
+
+cd out
+git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
+git rm -rf .
+cd ..
+
+yarn build
+
+cp -a build/. out/.
+
+mkdir -p out/.circleci && cp -a .circleci/. out/.circleci/.
+cd out
+
+git add -A
+git commit -m "Automated deployment to GitHub Pages: ${CIRCLE_SHA1}" --allow-empty
+
+git push origin $TARGET_BRANCH
