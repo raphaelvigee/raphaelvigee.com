24a0d5082aaf4d079afa24c2a3cc72b45eed58b4
Raphael Vigee <contact@raphaelvigee.com>
2018-11-26 00:06:11 -0800

    Fix ln path

--- .circleci/generate_structure.sh
+++ .circleci/generate_structure.sh
@@ -12,9 +12,14 @@ paths=(
 for p in "${paths[@]}"
 do
     dir=".$p"
+    file="$dir/index.html"
 
-    mkdir -p "$dir"
-    ln -s ./index.html "$dir/index.html"
+    if [[ -f ${file} ]]; then
+        rm ${file}
+    fi
+
+    mkdir -p ${dir}
+    ln -s ../index.html ${file}
 done
 
 ln -s ./index.html ./404.html
