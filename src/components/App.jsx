706fb6efb07dd0e43ca2db66952fb4143e98345f
Raphael Vigee <contact@raphaelvigee.com>
2018-11-25 21:27:04 -0800

    Add baseline responsiveness

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,35 +3,36 @@ import Sidebar from "./Sidebar";
 
 import styles from './App.scss';
 import Home from "./Home";
-import {BrowserRouter as Router, Redirect, Route, Switch, withRouter} from "react-router-dom";
+import {BrowserRouter as Router, Redirect, Route, Switch} from "react-router-dom";
 import Education from "./Education";
 import Experiences from "./Experiences";
 import ScrollToTop from "./ScrollTop";
 import Resume from "./Resume";
 import Contact from "./Contact";
+import cx from 'classnames';
 
 export default class App extends React.Component {
     render() {
         return (
             <Router>
                 <ScrollToTop>
-                    <div className={styles.app}>
-                        <div className={styles.left}>
-                            <Route path={'/'} exact children={({match}) => (
-                                <Sidebar homepage={!!match}/>
-                            )}/>
+                    <Route path={'/'} exact children={({match}) => (
+                        <div className={cx(styles.app, match && styles.homepage)}>
+                            <div className={styles.left}>
+                                <Sidebar homepage={match}/>
+                            </div>
+                            <div className={styles.right}>
+                                <Switch>
+                                    <Route path="/" exact component={Home}/>
+                                    <Route path="/education" exact component={Education}/>
+                                    <Route path="/experiences" exact component={Experiences}/>
+                                    <Route path="/resume" exact component={Resume}/>
+                                    <Route path="/contact" exact component={Contact}/>
+                                    <Route render={() => <Redirect to={'/'}/>}/>
+                                </Switch>
+                            </div>
                         </div>
-                        <div className={styles.right}>
-                            <Switch>
-                                <Route path="/" exact component={Home}/>
-                                <Route path="/education" exact component={Education}/>
-                                <Route path="/experiences" exact component={Experiences}/>
-                                <Route path="/resume" exact component={Resume}/>
-                                <Route path="/contact" exact component={Contact}/>
-                                <Route render={() => <Redirect to={'/'} />}/>
-                            </Switch>
-                        </div>
-                    </div>
+                    )}/>
                 </ScrollToTop>
             </Router>
         );
