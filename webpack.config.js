9bf42b692dc111f869e1e6627da4a55498eac8a0
Raphael Vigee <contact@raphaelvigee.com>
2018-11-26 00:41:38 -0800

    Add favicons

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const webpack = require('webpack');
 const Encore = require('@symfony/webpack-encore');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
 
 Encore
     .setOutputPath('build/')
@@ -52,5 +53,39 @@ config.plugins.push(
         },
     }),
 );
+
+config.plugins.push(new FaviconsWebpackPlugin({
+    // Your source logo
+    logo: `${__dirname}/src/assets/images/raccoon.png`,
+    // The prefix for all image files (might be a folder or a name)
+    prefix: 'icons-[hash]/',
+    // Emit all stats of the generated icons
+    emitStats: false,
+    // The name of the json containing all favicon information
+    statsFilename: 'iconstats-[hash].json',
+    // Generate a cache file with control hashes and
+    // don't rebuild the favicons until those hashes change
+    persistentCache: true,
+    // Inject the html into the html-webpack-plugin
+    inject: true,
+    // favicon background color (see https://github.com/haydenbleasel/favicons#usage)
+    background: '#fff',
+    // favicon app title (see https://github.com/haydenbleasel/favicons#usage)
+    title: 'Raphaël Vigee',
+
+    // which icons should be generated (see https://github.com/haydenbleasel/favicons#usage)
+    icons: {
+        android: true,
+        appleIcon: true,
+        appleStartup: true,
+        coast: false,
+        favicons: true,
+        firefox: true,
+        opengraph: false,
+        twitter: false,
+        yandex: false,
+        windows: false
+    }
+}));
 
 module.exports = config;
